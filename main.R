b262f6859fc147492b7f77d7f471c20c7934d194
mihai <25828335+MihaiGroza@users.noreply.github.com>
2023-12-08 10:01:00 -0500

    new data columns

--- main.R
+++ main.R
@@ -1,3 +1,55 @@
 library(tidyverse)
-source("data_pipeline.R")
-parse_retrosheet_pbp(1950)
+
+#load data
+fields <- read_csv("retrosheet/unzipped/fields.csv")
+data2016 <- read_csv("retrosheet/unzipped/all2016.csv",
+                     col_names = pull(fields, Header),
+                     na = character())
+
+
+data2016 %>%
+  mutate(RUNS = AWAY_SCORE_CT + HOME_SCORE_CT,
+         HALF.INNING = paste(GAME_ID, INN_CT, BAT_HOME_ID),
+         RUNS.SCORED =
+           (BAT_DEST_ID > 3) + (RUN1_DEST_ID > 3) +
+           (RUN2_DEST_ID > 3) + (RUN3_DEST_ID > 3)) ->
+  data2016
+
+data2016 %>%
+  group_by(HALF.INNING) %>%
+  summarize(Outs.Inning = sum(EVENT_OUTS_CT),
+            Runs.Inning = sum(RUNS.SCORED),
+            Runs.Start = first(RUNS),
+            MAX.RUNS = Runs.Inning + Runs.Start) ->
+  half_innings
+
+
+# data2016 %>%
+#   inner_join(half_innings, by = "HALF.INNING") %>%
+#   mutate(RUNS.ROI = MAX.RUNS - RUNS) ->
+#   data2016
+
+data2016 %>%
+  inner_join(half_innings, by = "HALF.INNING") %>%
+  mutate(BASES =
+           paste(ifelse(BASE1_RUN_ID > '', 1, 0),
+                 ifelse(BASE2_RUN_ID > '', 1, 0),
+                 ifelse(BASE3_RUN_ID > '', 1, 0), sep = ""),
+         STATE = paste(BASES, OUTS_CT),
+         NRUNNER1 =
+           as.numeric(RUN1_DEST_ID == 1 | BAT_DEST_ID == 1),
+         NRUNNER2 =
+           as.numeric(RUN1_DEST_ID == 2 | RUN2_DEST_ID == 2 |
+                        BAT_DEST_ID == 2),
+         NRUNNER3 =
+           as.numeric(RUN1_DEST_ID == 3 | RUN2_DEST_ID == 3 |
+                        RUN3_DEST_ID == 3 | BAT_DEST_ID == 3),
+         NOUTS = OUTS_CT + EVENT_OUTS_CT,
+         NEW.BASES = paste(NRUNNER1, NRUNNER2,
+                           NRUNNER3, sep = ""),
+         NEW.STATE = paste(NEW.BASES, NOUTS)) -> data2016
+
+
+
+
+
